d5c17edde9b58526d275947762d0d56f9b27df6e
alaga <alagarmca006@gmail.com>
2026-05-16 20:59:35 +0530

    initial commit-of E-card Mobile Store API.

--- src/main/java/com/example/ecommerce/model/Order.java
+++ src/main/java/com/example/ecommerce/model/Order.java
@@ -6,7 +6,7 @@ import java.time.LocalDateTime;
 
 @Entity
 @Data
-@Table(name = "cs_orders")
+@Table(name = "orders")
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/example/ecommerce/model/Product.java
+++ src/main/java/com/example/ecommerce/model/Product.java
@@ -5,7 +5,7 @@ import lombok.Data;
 
 @Entity
 @Data
-@Table(name = "cs_products")
+@Table(name = "products")
 public class Product {
 
     @Id
